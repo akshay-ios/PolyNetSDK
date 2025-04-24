eed46ecd8c706b9fb995ffcf4057416fb76ab431
Akshay Kumar <akshaykumar@Akshays-MacBook-Air.local>
2025-04-24 18:21:17 +0530

    update checksum and url

--- Package.swift
+++ Package.swift
@@ -1,24 +1,28 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version: 5.6
 import PackageDescription
 
 let package = Package(
     name: "PolyNetSDK",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PolyNetSDK",
-            targets: ["PolyNetSDK"]),
+            targets: ["PolyNetSDK"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", exact: "1.26.0"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.8")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "PolyNetSDK"),
-        .testTarget(
-            name: "PolyNetSDKTests",
-            dependencies: ["PolyNetSDK"]
-        ),
+        .binaryTarget(
+            name: "PolyNetSDK",
+            url:"https://artifacts.s73cloud.com/repository/maven-s73-releases/s73-polynet-plat/polynet-swift-package-manager/5.2.0/polynet-swift-package-manager-5.2.0.zip",
+            checksum: "461cc80e9e117a782438b330aaca1977d8d9ff6ebc4e04d490f082c98e37facd"
+        )
     ]
 )
+
